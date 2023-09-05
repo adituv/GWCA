da6980e20cc24138c7c5ae2a6ae110c31f045aab
AdituV <aditu.venyhandottir@gmail.com>
2023-09-05 13:34:31 +0100

    asdf

---
+++ Include/GWCA/Constants/ItemMod.h
@@ -0,0 +1,13 @@
+namespace GW {
+    namespace Constants {
+        namespace ModIdentifier {
+            constexpr int AddEnergy = 8920;
+            constexpr int HasModComponent = 9224;
+            constexpr int DamageType = 9440;
+            constexpr int Requirement = 10136;
+            constexpr int FocusEnergy = 26568;
+            constexpr int DamageRange = 42920;
+        }
+
+    }
+}
